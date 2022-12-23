978b00b7edae52fe53245d50d14d18c61a0042ad
saimonproyectsv2 <simon.patino@udea.edu.co>
2022-12-23 18:46:40 -0500

    add new functions for the movements

--- motor_1.ino
+++ motor_1.ino
@@ -11,10 +11,10 @@ void setup() {
   // put your setup code here, to run once:
 
 
- engine1.setSpeed(0);
- engine2.setSpeed(0);
- engine3.setSpeed(0);
- engine4.setSpeed(0);
+ engine1.setSpeed(255);
+ engine2.setSpeed(255);
+ engine3.setSpeed(255);
+ engine4.setSpeed(255);
 }
   
 // a void loop is always necesary on arduino. In case you forget it --->
@@ -22,17 +22,72 @@ void setup() {
 //collect2: error: ld returned 1 exit status
 
 void loop() {
-
-
+  turn_right();
+  
 }
+
+//void function doesn't return a value while function() always return a value 
+
 void forward() {
   
-
   engine2.run(BACKWARD);
   engine4.run(FORWARD);
   engine1.run(BACKWARD);
   engine3.run(FORWARD);
- 
 
 }
-//void function doesn't return a value while function() always return a value 
+
+void backward() {
+  
+  engine2.run(FORWARD);
+  engine4.run(BACKWARD);
+  engine1.run(FORWARD);
+  engine3.run(BACKWARD);
+
+}
+
+void forward_top() {
+  
+  engine2.run(RELEASE);
+  engine4.run(FORWARD);
+  engine1.run(RELEASE);
+  engine3.run(FORWARD);
+
+}
+
+void forward_bottom() {
+  
+  engine2.run(BACKWARD);
+  engine4.run(RELEASE);
+  engine1.run(BACKWARD);
+  engine3.run(RELEASE);
+  
+}
+
+void turn_left() {
+
+  engine1.setSpeed(100);
+  engine2.setSpeed(255);
+  engine3.setSpeed(255);
+  engine4.setSpeed(100);  
+  
+  engine2.run(BACKWARD);
+  engine4.run(FORWARD);
+  engine1.run(BACKWARD);
+  engine3.run(FORWARD);
+
+}
+
+void turn_right() {
+
+  engine1.setSpeed(255);
+  engine2.setSpeed(60);
+  engine3.setSpeed(60);
+  engine4.setSpeed(255);  
+  
+  engine2.run(BACKWARD);
+  engine4.run(FORWARD);
+  engine1.run(BACKWARD);
+  engine3.run(FORWARD);
+
+}
